e88a720c20ee136e13d915a869dcb6f65a68e861
Z <444835695@qq.com>
2024-05-01 12:43:12 +0800

    创建商品模块

---
+++ gulimall-product/src/main/java/cn/gulimall/demo/Application.java
@@ -0,0 +1,8 @@
+package cn.gulimall.demo;
+
+/**
+ * @author Z
+ * @date 2024/5/1 12:41
+ */
+public class Application {
+}
---
+++ gulimall-product/src/main/java/cn/gulimall/demo/controller/TestController.scala
@@ -0,0 +1,10 @@
+package cn.gulimall.demo.controller
+
+/**
+ *
+ * @author Z
+ * @date 2024/5/1 12:42
+ */
+class TestController {
+
+}
