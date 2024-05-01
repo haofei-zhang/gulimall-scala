e9c1950eb0d277485d5cc4eaafb93283ba6b3ad6
Z <444835695@qq.com>
2024-05-01 12:48:56 +0800

    test接口

--- gulimall-product/src/main/java/cn/gulimall/demo/Application.java
+++ gulimall-product/src/main/java/cn/gulimall/demo/Application.java
@@ -1,8 +1,16 @@
 package cn.gulimall.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Z
  * @date 2024/5/1 12:41
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- gulimall-product/src/main/java/cn/gulimall/demo/controller/TestController.scala
+++ gulimall-product/src/main/java/cn/gulimall/demo/controller/TestController.scala
@@ -1,10 +1,19 @@
 package cn.gulimall.demo.controller
 
+import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RestController}
+
 /**
  *
  * @author Z
  * @date 2024/5/1 12:42
  */
+@RestController
+@RequestMapping(Array("/test"))
 class TestController {
+
+  @GetMapping(Array("/test1"))
+  def test1(): Unit = {
+    println("test1")
+  }
 
 }
