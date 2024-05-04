5746db6ef524f61b7633af9a32b7e19b5818bd54
Z <z444835695@163.com>
2024-05-04 23:59:50 +0800

    管理模块启动类

---
+++ gulimall-admin/src/main/java/cn/gulimall/demo/AdminApplication.java
@@ -0,0 +1,15 @@
+package cn.gulimall.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Z
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
---
+++ gulimall-admin/src/main/java/cn/gulimall/demo/controller/SysLoginController.scala
@@ -0,0 +1,18 @@
+package cn.gulimall.demo.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @author Z
+ */
+@RestController
+object SysLoginController {
+
+  def login(): Unit = {
+  }
+
+
+
+
+
+}
