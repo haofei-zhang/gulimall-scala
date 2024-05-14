d774ef683731aa8ca7c5b82f992eed265d1e4ef5
Z <444835695@qq.com>
2024-05-14 22:47:59 +0800

    使用scala写启动类

---
+++ gulimall-admin/src/main/java/cn/gulimall/demo/AdminApplication.scala
@@ -0,0 +1,16 @@
+package cn.gulimall.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ *
+ * @author Z
+ * @since 2024/5/14 下午10:37
+ */
+@SpringBootApplication
+class AdminApplication
+
+object AdminApplication extends App {
+  SpringApplication.run(classOf[AdminApplication], args: _*)
+}
