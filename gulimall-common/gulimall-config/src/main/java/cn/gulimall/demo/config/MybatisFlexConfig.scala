aff5f88252509308595c04a510ff64fbee11ed0d
haofei-zhang <444835695@qq.com>
2024-08-08 00:22:31 +0800

    优化配置

--- gulimall-common/gulimall-config/src/main/java/cn/gulimall/demo/config/MybatisFlexConfig.scala
+++ gulimall-common/gulimall-config/src/main/java/cn/gulimall/demo/config/MybatisFlexConfig.scala
@@ -1,8 +1,7 @@
 package cn.gulimall.demo.config
 
 import com.mybatisflex.core.FlexGlobalConfig
-import org.mybatis.spring.annotation.MapperScan
-import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.context.annotation.Configuration
 
 /**
  * 2024/5/12 下午2:06
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.{Bean, Configuration}
  * @author 44483
  */
 @Configuration
-@MapperScan(Array("cn.gulimall.demo.mapper"))
 class MybatisFlexConfig {
 
   FlexGlobalConfig.getDefaultConfig.setLogicDeleteColumn("del")
